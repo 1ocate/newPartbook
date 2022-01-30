dcdd0990fa3ad517c44447ba7c3711c8db79e968
Jay Yeo <integrityms@gmail.com>
2022-01-31 00:28:54 +0700

    add meta tag for serach and share, google analitics

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,6 +1,17 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
+
+        <!-- Global site tag (gtag.js) - Google Analytics -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-17MSL1BZRK"></script>
+        <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-17MSL1BZRK');
+        </script>
+
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -1,10 +1,31 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
+
+        <!-- Global site tag (gtag.js) - Google Analytics -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-17MSL1BZRK"></script>
+        <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-17MSL1BZRK');
+        </script>
+
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
+
+         <!-- meta for share or search -->
+        <meta property="og:url" content="https://partbook.co.id/">
+        <meta property="og:title" content="PARTBOOK.ID">
+        <meta property="og:type" content="website">
+        <meta property="og:image" content="{{ secure_asset('favicon/apple-icon-180x180.png')}}">
+        <meta property="og:description" content="site pertama referensi part no yang akurat dengan standard international.">
+
+        <meta name="description" content="site pertama referensi part no yang akurat dengan standard international.">
+        <meta name="keywords" content="partbook,part">
 
         <title>{{ config('app.name', 'Laravel') }}</title>
 
