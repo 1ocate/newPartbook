04772597df76b2f30d24e15cb2751645270e0139
Jay Yeo <integrityms@gmail.com>
2022-02-03 16:40:46 +0700

    Change view index

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -11,9 +11,13 @@
                 Refensi Partbook untuk Mesin produksi anda dan Excell generator <br /><br />
                 Database dan Informasi Sparepart Mesin Produksi, beserta No Part terlengkap dan terupdate yang dapat bantu Kinerja Produksi, juga membantu dalam mempercepat untuk membuat list excel kebutuhan sparepart
             </p>
-            <div class="text-center">
-                <a href="/register" class="items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:ring ring-gray-300 disabled:opacity-25 transition ease-in-out duration-150 ml-4">Daftar Sekarang</a>
+            <div class="text-center mb-5">
+                <a href="/login" class="items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:ring ring-gray-300 disabled:opacity-25 transition ease-in-out duration-150">Daftar Sekarang</a>
             </div>
+            <div class="text-center">
+                <img class="inline" src="{{ asset('img/example.gif') }}" />
+            </div>
+
         </div>
 
        
